888b3576f99133ae1c2db9f85200c01efab5c285
Alex Blair <bclan11@gmail.com>
2022-01-13 10:27:11 -0800

    test passing

--- test/example.test.js
+++ test/example.test.js
@@ -1,6 +1,8 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { renderBunny } from '../families/render-utils.js';
+
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
@@ -14,5 +16,18 @@ test('time to test a function', (expect) => {
 
     //Expect
     // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('this function should create an h4 dom element, add class list and text content and return the element', (expect) => {
+
+    const bunny = {
+        name: 'Fred'
+    };
+
+    const expected = '<h4 class="bunny">Fred</h4>';
+
+    const actual = renderBunny(bunny).outerHTML;
+
     expect.equal(actual, expected);
 });
